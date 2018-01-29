b8415ebcb68abc2520020bb1a163d72659a6905d
Peter Ahlgren <ahlgren1234@gmail.com>
2018-01-29 11:11:18 +0100

    Added a testfile

---
+++ testfile.php
@@ -0,0 +1,33 @@
+<?php
+
+function die_r($value) {
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+    die();
+}
+
+require_once 'Database.php';
+
+$db = new Database();
+
+// getRow.
+// $getRow = $db->getRow("SELECT * FROM users WHERE id = ?", ["2"]);
+// die_r($getRow);
+
+// getRows.
+// $getRows = $db->getRows("SELECT * FROM users WHERE username = ?", ["JohnDoe"]);
+// die_r($getRows);
+
+// insertRow
+// $insertRow = $db->insertRow("INSERT INTO users(username, password, email) VALUE(?, ?, ?)", ["BillGates", "password123", "bill@microsoft.com"]);
+// die_r($insertRow);
+
+// updateRow
+// $updateRow = $db->updateRow("UPDATE users SET username = ?, password = ? WHERE id = ?", ["SteveJobs", "MacRules", "2"]);
+// die_r($updateRow);
+
+// deleteRow
+$deleteRow = $db->deleteRow("DELETE FROM users WHERE id = ?", ["1"]);
+die_r($deleteRow);
+?>
